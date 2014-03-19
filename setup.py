92b6b08f36a9a2cf44dcfd70251bed4c280b3951
wenpeng zou <wen@tailwindweb.com>
2014-03-19 13:34:14 -0700

    add more messages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='UDS Module',
-    version='0.1.0',
+    version='0.1.1',
     author='Wen Zou',
     author_email='wen@independa.com',
     packages=['validate'],
--- validate/validation.py
+++ validate/validation.py
@@ -6,20 +6,20 @@ from constants import *
 def validate(value, reading_type, unit):
     #check if reading_type exists
     if reading_type not in READING_TYPE_SLUG_LIST:
-        return False
+        return {'success': False, 'message': 'Invalid reading type'}
 
     #check if unit matches the reading_type
     if unit not in ACCEPTED_UNITS_BY_READING_TYPE[reading_type]:
-        return False
+        return {'success': False, 'message': 'Invalid unit for reading type'}
     #check if value within acceptable range of unit
     MIN_INDEX = 0
     MAX_INDEX = 1
     if unit == BOOLEAN_UNIT_SLUG:
         if value == RANGE_SPEC_BY_UNIT[BOOLEAN_UNIT_SLUG][MIN_INDEX] or value == RANGE_SPEC_BY_UNIT[BOOLEAN_UNIT_SLUG][MAX_INDEX]:
-            return True
+            return {'success': True, 'message': 'Successful'}
     else:
         if RANGE_SPEC_BY_UNIT[unit][MIN_INDEX] < value < RANGE_SPEC_BY_UNIT[unit][MAX_INDEX]:
-            return True
+            return {'success': True, 'message': 'Successful'}
 
     #return false if anything fails
-    return False
+    return {'success': False, 'message': 'value range invalid for unit'}
