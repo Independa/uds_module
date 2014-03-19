8540ca445d965b5e8434212ace1938deec478e44
wenpeng zou <wen@tailwindweb.com>
2014-03-19 13:59:06 -0700

    updated version

--- README.txt
+++ README.txt
@@ -1,1 +1,10 @@
 This is the module for the UDS
+
+
+
+
+
+
+References:
+http://guide.python-distribute.org/creation.html
+http://www.pip-installer.org/en/1.1/usage.html
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='UDS Module',
-    version='0.1.1',
+    version='0.1.2',
     author='Wen Zou',
     author_email='wen@independa.com',
     packages=['validate'],
