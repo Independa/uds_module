1a9187f4fbb5b85a573d8da64359fae53deacc57
wenpeng zou <wen@tailwindweb.com>
2014-03-19 12:56:09 -0700

    added constant and validation function

--- validate/validation.py
+++ validate/validation.py
@@ -1,5 +1,25 @@
 __author__ = 'wenzou'
 
+from constants import *
 
-def validate():
-    print "validate"
+
+def validate(value, reading_type, unit):
+    #check if reading_type exists
+    if reading_type not in READING_TYPE_SLUG_LIST:
+        return False
+
+    #check if unit matches the reading_type
+    if unit not in ACCEPTED_UNITS_BY_READING_TYPE[reading_type]:
+        return False
+    #check if value within acceptable range of unit
+    MIN_INDEX = 0
+    MAX_INDEX = 1
+    if unit == BOOLEAN_UNIT_SLUG:
+        if value == RANGE_SPEC_BY_UNIT[BOOLEAN_UNIT_SLUG][MIN_INDEX] or value == RANGE_SPEC_BY_UNIT[BOOLEAN_UNIT_SLUG][MAX_INDEX]:
+            return True
+    else:
+        if RANGE_SPEC_BY_UNIT[unit][MIN_INDEX] < value < RANGE_SPEC_BY_UNIT[unit][MAX_INDEX]:
+            return True
+
+    #return false if anything fails
+    return False
