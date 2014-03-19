336b4727e86ed1a718237f3a92d04f85da7aad65
wenpeng zou <wen@tailwindweb.com>
2014-03-18 20:51:21 -0700

    added setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='TowelStuff',
+    version='0.1.0',
+    author='Wen Zou',
+    author_email='wen@independa.com',
+    packages=['validate'],
+    scripts=[''],
+    url='',
+    license='LICENSE.txt',
+    description='This is UDS module for data validation and queue sender',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "Django >= 1.1.1",
+        ],
+    )
