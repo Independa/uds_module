3390558d5577e86fdf0d4cbbf02f8522bdea4e7b
Peter Hsieh <peter@tailwindweb.com>
2014-03-21 12:39:15 -0700

    Increment module package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='UDS Readings Module',
-    version='0.1.4',
+    version='0.1.5',
     author='Independa Inc,',
     author_email='wen@independa.com',
     packages=['validate'],
