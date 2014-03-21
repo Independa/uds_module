712c28f4e3fc739be150f65ffb47480fd7ca5568
Peter Hsieh <peter@tailwindweb.com>
2014-03-21 13:42:11 -0700

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='UDS Readings Module',
-    version='0.1.11',
+    version='0.1.12',
     author='Independa Inc,',
     author_email='wen@independa.com',
     packages=['validate'],
