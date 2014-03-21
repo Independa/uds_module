a2f18107fe34dab44325deedeb893aac166f9a8b
Peter Hsieh <peter@tailwindweb.com>
2014-03-21 13:10:05 -0700

    bumping version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='UDS Readings Module',
-    version='0.1.8',
+    version='0.1.9',
     author='Independa Inc,',
     author_email='wen@independa.com',
     packages=['validate'],
